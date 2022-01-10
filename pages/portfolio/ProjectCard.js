1d92ffc0e32d1e722184c2d47730a430bcac9bbd
christian.wickerstrom@gmail.com <christian.wickerstrom@gmail.com>
2022-01-10 07:21:51 +0100

    adding passHref to projectcard

--- pages/portfolio/ProjectCard.js
+++ pages/portfolio/ProjectCard.js
@@ -5,7 +5,7 @@ function WorkExperience({ project }) {
     const { title, tagLine, tags } = project?.fields || {};
 
     return <div className={styles.projectCard}>
-        <Link href={`/portfolio/${project?.fields?.slug ? project?.fields?.slug : null}`}>
+        <Link href={`/portfolio/${project?.fields?.slug ? project?.fields?.slug : null}`} passHref={true}>
             <div>
                 <a>
                     {title}
