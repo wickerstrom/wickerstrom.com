14fad99eead764722bbb5a0b993b920e0a7610cf
christian.wickerstrom@gmail.com <christian.wickerstrom@gmail.com>
2022-01-30 12:17:35 +0100

    mobile drawer styling update

--- components/navbar/MobileDrawer.js
+++ components/navbar/MobileDrawer.js
@@ -3,20 +3,21 @@ import { useState } from "react";
 import styles from '../../styles/navbar.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
 
 function MobileDrawer(props) {
     const { navbarConfig } = props;
     const [isActive, setIsActive] = useState(false);
 
     return <div className={styles.mobileDrawerWrapper}>
-        {!isActive &&
-            <div className={styles.mobileDrawerIcon}>
-                <FontAwesomeIcon icon={faBars} size={'2x'} onClick={() => setIsActive(!isActive)} />
-            </div>}
+
+        <div className={styles.mobileDrawerIcon}>
+            <FontAwesomeIcon icon={faBars} size={'2x'} onClick={() => setIsActive(!isActive)} />
+        </div>
         {isActive &&
             <div className={styles.activeMobileWrapper}>
                 <div className={styles.mobileDrawerIcon}>
-                    <FontAwesomeIcon icon={faBars} size={'2x'} onClick={() => setIsActive(!isActive)} />
+                    <FontAwesomeIcon icon={faTimes} size={'2x'} onClick={() => setIsActive(!isActive)} />
                 </div>
                 <div className={styles.mobileNavLinks}>
                     <ul>
