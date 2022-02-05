217d68aaa9680e73883383bb1ff067ac6022f9cd
christian.wickerstrom@gmail.com <christian.wickerstrom@gmail.com>
2022-02-05 12:30:17 +0100

    adding hotjar support

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
 import '../styles/globals.css'
+import { useEffect } from 'react'
+import { hotjar } from 'react-hotjar'
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    hotjar.initialize(2816753, 6);
+  }, [])
+
   return <Component {...pageProps} />
 }
 
